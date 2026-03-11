1b95bd41d90e80aecad7ee152bdb49eb8745f159
clawtribe.ai <support@jmalltech.com>
2026-03-11 17:58:24 +0800

    🔖 修复注册命令时的静默假死感并升级至 v3.3.2

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = 'Stop'
 $global:Success = $false
 
 # --- 基础配置变量 ---
-$global:Version = '3.3.1'
+$global:Version = '3.3.2'
 $global:RepoUser = 'ClawTribe'
 $global:RepoName = 'openclaw-oneclick'
 $global:InstallDir = Join-Path $HOME 'OpenClaw'
