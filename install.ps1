3b0cdfba7e4a0bb66daf29070d24a3d3832cbbc5
clawtribe.ai <support@jmalltech.com>
2026-03-11 11:08:11 +0800

    更新版本

--- install.ps1
+++ install.ps1
@@ -4,7 +4,7 @@
 $ErrorActionPreference = 'Continue'
 $global:Success = $false
 
-$Version = '3.1.0'
+$Version = '3.1.1'
 $RepoUser = 'ClawTribe'
 $RepoName = 'openclaw-oneclick'
 $InstallDir = Join-Path $HOME 'OpenClaw'
