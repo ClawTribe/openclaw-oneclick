db23ec6739bb5029bb5c3939661b1f3858c01915
wanghanting <hanting.wang@jmalltech.com>
2026-03-13 21:52:30 +0800

    🔖 Bump version to 3.3.16

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ log() {
 }
 
 # --- 基础配置变量 ---
-export VERSION="3.3.15"
+export VERSION="3.3.16"
 export REPO_USER="ClawTribe"
 export REPO_NAME="openclaw-oneclick"
 export INSTALL_DIR="$HOME/OpenClaw"
